ee3459f71e85d04cee99240a87a95aef398dfb96
juanfel254 <jparrar@unal.edu.co>
2023-06-06 00:56:40 -0500

    Kennedy UPZs and map styles first added.

--- src/components/kennedyMap/kennedyMap.js
+++ src/components/kennedyMap/kennedyMap.js
@@ -7,28 +7,37 @@ mapboxgl.accessToken = 'pk.eyJ1IjoianVhbmZlbDI1NCIsImEiOiJjbGlkYnpsdHYwMWUxM21tb
 export default function KennedyMap() {
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const [lng, setLng] = useState(-74.1725814);
-  const [lat, setLat] = useState(4.6299331);
-  const [zoom, setZoom] = useState(12);
-
+  const [lng, setLng] = useState(-74.1531818);
+  const [lat, setLat] = useState(4.6299322);
+  const [zoom, setZoom] = useState(12.25);
+  const bounds = [
+    [-74.04728, 40.68392], // [west, south]
+    [-73.91058, 40.87764]  // [east, north]
+    ];
+    
   useEffect(() => {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
         container: mapContainer.current,
-        style: 'mapbox://styles/mapbox/streets-v12',
+        //style: 'mapbox://styles/juanfel254/clijtxv4900ug01pddglfckk8',
+        style: 'mapbox://styles/juanfel254/clijutcu700v001p71axde6d0',
         center: [lng, lat],
         zoom: zoom
       });
+    
+    map.current.setMinZoom(zoom);
+    map.current.setMaxZoom(13);
+    //map.current.addControl(new mapboxgl.NavigationControl());
     });
 
 
   useEffect(() => {
     if (!map.current) return; // wait for map to initialize
-      
-      map.current.on('zoom', () => {
-        setZoom(12);
-      })
 
+/*       map.current.on('zoom', () => {
+        map.current.setZoom(zoom);
+      })
+ */
       map.current.on('move', () => {
         setLng(map.current.getCenter().lng.toFixed(4));
         setLat(map.current.getCenter().lat.toFixed(4));
