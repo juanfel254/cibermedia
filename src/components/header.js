cfc6096aa5f525c488fd225d3fa5ab37abbaf91b
juanfel254 <jparrar@unal.edu.co>
2023-06-15 13:43:35 -0500

    Header adaptation to react completed

--- src/components/header.js
+++ src/components/header.js
@@ -1,39 +1,77 @@
+import React, { useState } from "react";
+import styles from "@/styles/header.module.css";
 import Link from "next/link";
 import Image from 'next/image';
-import styles from "@/styles/header.module.css";
-import { Rubik } from 'next/font/google';
 
-const rubik = Rubik({ subsets: ['latin'] });
+const menuItems = [
+  { label: "Mapa", content: "¡Aquí estamos!", link: "/" },
+  { label: "Artistas", content: "Lo que hemos logrado", link: "/artistas" },
+  { label: "Nosotros", content: "¿Quiénes somos?", link: "/nosotros" },
+  { label: "Contacto", content: "Te escuchamos", link: "/contacto" }
+];
 
-export default function Header () {
+export default function Header() {
+  const [isActive, setIsActive] = useState(false);
+  const handleOverlayClick = () => setIsActive(!isActive);
+
   return (
-    <header className={`${styles.header} ${rubik.className}`}>
-      <Link href="/" className={`my-link ${styles.home_logo}`}>
-        <ul className={styles.home_logo_secondary_container}>
-          <Image
-            src="/main-logos/icono-cibermedia-morado.svg"
-            width={180}
-            height={90}
-            alt="Logo del Colectivo Cultural Cibermedia"
-            className="header-logo"
-          />
-          <h3>Colectivo Cultural Cibermedia</h3>
-        </ul>
-      </Link>
-      <ul>
-        <li>
-          <Link href="/" className="my-link">Mapa</Link>
-        </li>
-        <li>
-          <Link href="/artistas" className="my-link">Artistas</Link>
-        </li>
-        <li>
-          <Link href="/nosotros" className="my-link">Nosotros</Link>
-        </li>
-        <li>
-          <Link href="/contacto" className="my-link">Contacto</Link>
-        </li>
-      </ul>
-    </header>
+    <>
+      <div
+        className={`overlay-navigation ${
+          isActive ? "overlay-active overlay-slide-down" : "overlay-slide-up"
+        }`}
+      >
+        <nav role="navigation">
+          <ul>
+            {menuItems.map((item, index) => (
+              <li
+                className={
+                  isActive
+                    ? `slide-in-nav-item-delay-${index}`
+                    : `slide-in-nav-item-delay-${index}-reverse`
+                }
+                key={`menuitem-${index}`}
+              >
+                <Link href={`${item.link}`} data-content={item.content}>
+                  {item.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+
+      <section className="header">
+        <Link href="/" className={`my-link ${styles.home_logo}`}>
+          <ul className={styles.home_logo_secondary_container}>
+            <Image
+              src="/main-logos/icono-cibermedia-morado.svg"
+              width={180}
+              height={90}
+              alt="Logo del Colectivo Cultural Cibermedia"
+              className="header-logo"
+            />
+            <h3>Colectivo Cultural Cibermedia</h3>
+          </ul>
+        </Link>
+        <div className="open-overlay" onClick={handleOverlayClick}>
+          <span
+            className={`bar-top ${
+              isActive ? "animate-top-bar" : "animate-out-top-bar"
+            }`}
+          ></span>
+          <span
+            className={`bar-middle ${
+              isActive ? "animate-middle-bar" : "animate-out-middle-bar"
+            }`}
+          ></span>
+          <span
+            className={`bar-bottom ${
+              isActive ? "animate-bottom-bar" : "animate-out-bottom-bar"
+            }`}
+          ></span>
+        </div>
+      </section>
+    </>
   );
-}
+};
