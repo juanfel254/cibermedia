5bad58549ed521ee88aff6cff209b73e0a42c3d3
juanfel254 <jparrar@unal.edu.co>
2023-07-03 15:06:42 -0500

    Transition between profile images added

--- src/pages/artistas/[slug].js
+++ src/pages/artistas/[slug].js
@@ -1,61 +1,98 @@
-import Head from "next/head"
-import Image from "next/image"
-import styles from "@/styles/pages-styles/artista.module.css"
+import Head from "next/head";
+import Image from "next/image";
+import styles from "@/styles/pages-styles/artista.module.css";
 import Link from "next/link";
-import Tilt from 'react-parallax-tilt';
+import Tilt from "react-parallax-tilt";
 import { useEffect, useRef, useState } from "react";
 import AudioPlayer from "@/components/audioPlayer/AudioPlayer";
 
 export const getStaticPaths = async () => {
-  const res = await fetch('https://admin.ciberespacioartistico.com/index.php/wp-json/wp/v2/portafolio?per_page=100');
+  const res = await fetch(
+    "https://admin.ciberespacioartistico.com/index.php/wp-json/wp/v2/portafolio?per_page=100"
+  );
   const data = await res.json();
-  const paths = data.map(artista => {
+  const paths = data.map((artista) => {
     return {
-      params: { slug: artista.slug }
-    }
-  })
+      params: { slug: artista.slug },
+    };
+  });
 
   return {
     paths,
-    fallback: false
-  }
-}
+    fallback: false,
+  };
+};
 
 export const getStaticProps = async (context) => {
   const slug = context.params.slug;
-  const url = 'https://admin.ciberespacioartistico.com/index.php/wp-json/wp/v2/portafolio?slug=' + slug;
+  const url =
+    "https://admin.ciberespacioartistico.com/index.php/wp-json/wp/v2/portafolio?slug=" +
+    slug;
   const res = await fetch(url);
   const data = await res.json();
-  return {  
-    props: { artista: data[0] }}
-}
+  return {
+    props: { artista: data[0] },
+  };
+};
 
-export default function ArtistaIndv({ artista }){
-
-  const [artistPicture, setArtistPicture] = useState(artista.ACF.galeria_fotos.foto_1)
+export default function ArtistaIndv({ artista }) {
   const [returnButton, setReturnButton] = useState(false);
 
-  const getGold = (youTubeLink) => youTubeLink.split("/")[3]
+  const [currentPhotoIndex, setCurrentPhotoIndex] = useState(0);
 
-  useEffect(()=> {
-    if(window.innerWidth < 1050) {
-      setReturnButton(true)
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // Obtener las claves de las fotos que contienen una URL
+      const photoKeys = Object.keys(artista.ACF.galeria_fotos).filter(
+        (key) => typeof artista.ACF.galeria_fotos[key] === "string"
+      );
+      setCurrentPhotoIndex((prevIndex) => (prevIndex + 1) % photoKeys.length);
+    }, 4000);
+
+    return () => {
+      // Limpiar el intervalo cuando el componente se desmonte
+      clearInterval(interval);
+    };
+  }, [artista.ACF.galeria_fotos]);
+
+  const photoKeys = Object.keys(artista.ACF.galeria_fotos).filter(
+    (key) => typeof artista.ACF.galeria_fotos[key] === "string"
+  );
+  const currentPhotoKey = photoKeys[currentPhotoIndex];
+  const currentPhotoUrl = artista.ACF.galeria_fotos[currentPhotoKey];
+
+  const getGold = (youTubeLink) => youTubeLink.split("/")[3];
+
+  useEffect(() => {
+    if (window.innerWidth < 1050) {
+      setReturnButton(true);
     } else {
-      setReturnButton(false)
+      setReturnButton(false);
     }
-  }, [returnButton])
+  }, [returnButton]);
 
   const renderRedesSocialesA = (redesSociales) => {
     if (redesSociales) {
       const enlacesRedesSociales = Object.entries(redesSociales)
-        .filter(([redSocial, data]) => typeof redSocial === 'string' && data !== null) // Filtrar las claves de cadena y datos no nulos
+        .filter(
+          ([redSocial, data]) => typeof redSocial === "string" && data !== null
+        ) // Filtrar las claves de cadena y datos no nulos
         .map(([redSocial, data]) => {
           if (data) {
-            const redSocialName = redSocial.replace(/\_\d+$/, ''); // Eliminar el número al final si existe
+            const redSocialName = redSocial.replace(/\_\d+$/, ""); // Eliminar el número al final si existe
             return (
-              <Link href={redSocialName === 'whatsapp' ? `https://wa.me/${data}` : data.url} className={`${styles.social_network} my-link`} target="_blank" key={redSocial}>
+              <Link
+                href={
+                  redSocialName === "whatsapp"
+                    ? `https://wa.me/${data}`
+                    : data.url
+                }
+                className={`${styles.social_network} my-link`}
+                target="_blank"
+                key={redSocial}
+              >
                 <Tilt scale={1.3}>
-                  <Image
+                  <img
                     src={`/social/${redSocialName}-morado.svg`}
                     alt={`ícono de ${redSocialName}`}
                     width={70}
@@ -64,69 +101,83 @@ export default function ArtistaIndv({ artista }){
                 </Tilt>
               </Link>
             );
-          } return null
-        }); return <div className={styles.artist_networks}>{enlacesRedesSociales}</div>;
-    } return null;
+          }
+          return null;
+        });
+      return (
+        <div className={styles.artist_networks}>{enlacesRedesSociales}</div>
+      );
+    }
+    return null;
   };
 
   return (
     <>
       <Head>
         <title>{artista.ACF.nombre_artista}</title>
-        <meta name="description" content={`Portafolio de ${artista.ACF.nombre_artista}: ${artista.ACF.descripcion_artista}`} />
+        <meta
+          name="description"
+          content={`Portafolio de ${artista.ACF.nombre_artista}: ${artista.ACF.descripcion_artista}`}
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+
       <div className={`${styles.portfolio_container} main-container`}>
         <div className={styles.artist_desc_auxiliar_container}>
-        <section className={`${styles.artist_desc_container}`} >
+          <section className={`${styles.artist_desc_container}`}>
+            <Link href="/artistas" className={`my-link ${styles.back_link}`}>
+              {"<<"}
+            </Link>
+            <center>
+              <Tilt scale={1.05} gyroscope={true}>
+                {photoKeys.map((key, index) => (
+                  <img
+                    key={key}
+                    src={artista.ACF.galeria_fotos[key]}
+                    width={300}
+                    height={300}
+                    alt="artist image"
+                    className={`${styles.artist_picture} ${
+                      index === currentPhotoIndex ? "" : styles.hidden_picture
+                    }`}
+                    id="artist-desc"
+                  />
+                ))}
+              </Tilt>
+            </center>
 
-        <Link href="/artistas" className={`my-link ${styles.back_link}`}>{"<<"}</Link>
-          <center>
-            <Tilt scale={1.05} gyroscope={true}>
-            <Image 
-              src={artistPicture} 
-              width={300}
-              height={300}
-              alt="artist image"
-              className={styles.artist_picture}
-              id="artist-desc"
-              />
-            </Tilt>
-          </center>
+            <h2 className={styles.artist_name}>{artista.title.rendered}</h2>
 
-          <h2 className={styles.artist_name}>
-            {artista.title.rendered}
-          </h2>
+            <div className={styles.artist_tags}>
+              {artista.perfil.map((tag, index) => {
+                return (
+                  <Tilt scale={1.2} tiltReverse={true} key={index}>
+                    <p className={styles.tagB}>#{tag}</p>
+                  </Tilt>
+                );
+              })}
+            </div>
+            <div className={styles.artist_text_desc}>
+              <p className={styles.artist_text}>
+                {artista.ACF.descripcion_artista}
+              </p>
+            </div>
 
-          <div className={styles.artist_tags}>
-            {artista.perfil.map((tag, index) => {
-              return <Tilt scale={1.2} tiltReverse={true} key={index}><p className={styles.tagB} >#{tag}</p></Tilt>
-            })}
-          </div>
+            <div className={styles.email_container}>
+              <p className={styles.email}>
+                {artista.ACF.correo_electronico
+                  ? artista.ACF.correo_electronico
+                  : null}
+              </p>
+            </div>
 
-{/*           <div className={styles.artist_tags}>
-            {artista.perfil.map((tag, index) => {
-              return <Tilt tiltReverse={true} key={index}><p className={styles.tag} >#{tag}</p></Tilt>
-            })}
-          </div> */}
-
-          <div className={styles.artist_text_desc}>
-            <p className={styles.artist_text}>{artista.ACF.descripcion_artista}</p>
-          </div>
-
-          <div className={styles.email_container}>
-            <p className={styles.email}>{artista.ACF.correo_electronico ? artista.ACF.correo_electronico : null}</p>
-          </div>
-
-          {renderRedesSocialesA(artista.acf.redes_sociales)}
-          
-        </section>
+            {renderRedesSocialesA(artista.acf.redes_sociales)}
+          </section>
         </div>
-        
+
         <ul className={styles.projects_container}>
-        {/* returnButton ? 
+          {/* returnButton ? 
               <Link href={"#artist-desc"} className={styles.return_button}>
                 <Image
                   alt="Botón de retorno"
@@ -143,26 +194,31 @@ export default function ArtistaIndv({ artista }){
           </li>
           <li className={styles.visual_project_container}>
             <div>
-              <iframe width="100%" height="501vw" src={"https://www.youtube.com/embed/"+getGold(artista.ACF.youtube_link)} title={artista.ACF.nombre_del_proyecto} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+              <iframe
+                width="100%"
+                height="501vw"
+                src={
+                  "https://www.youtube.com/embed/" +
+                  getGold(artista.ACF.youtube_link)
+                }
+                title={artista.ACF.nombre_del_proyecto}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen
+              ></iframe>
             </div>
-            <p className={styles.visual_desc}>
-              {artista.ACF.sinopsis_video}
-            </p>
-            <p>
-              {artista.ACF.creditos_video}
-            </p>
+            <p className={styles.visual_desc}>{artista.ACF.sinopsis_video}</p>
+            <p>{artista.ACF.creditos_video}</p>
           </li>
           <li className={styles.sound_project_container}>
-            {artista.ACF.archivo_audio && <AudioPlayer src={artista.ACF.archivo_audio}/>}
-            <p className={styles.sound_desc}>
-              {artista.ACF.sinopsis_audio}
-            </p>
-            <p>
-            {artista.ACF.creditos_audio}
-            </p>
+            {artista.ACF.archivo_audio && (
+              <AudioPlayer src={artista.ACF.archivo_audio} />
+            )}
+            <p className={styles.sound_desc}>{artista.ACF.sinopsis_audio}</p>
+            <p>{artista.ACF.creditos_audio}</p>
           </li>
         </ul>
       </div>
     </>
-  )
+  );
 }
