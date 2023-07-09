2b59e5583e46414a5963dd659c7f2ac66c231ace
juanfel254 <jparrar@unal.edu.co>
2023-07-08 21:43:34 -0500

    Credits for website added in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,29 +1,29 @@
 import Image from "next/image";
 import Link from "next/link";
 import styles from "@/styles/footer.module.css";
-import { Rubik } from 'next/font/google';
+import { Rubik } from "next/font/google";
 import { useEffect, useState } from "react";
 
-const rubik = Rubik({ subsets: ['latin'] });
+const rubik = Rubik({ subsets: ["latin"] });
 
-export default function Footer () {
+export default function Footer() {
+  const [data, setData] = useState("");
 
-  const [data, setData] = useState('');
-
-  useEffect(()=> {
-    async function fetchData () {
+  useEffect(() => {
+    async function fetchData() {
       try {
-        const url  = "https://admin.ciberespacioartistico.com/index.php/wp-json/wp/v2/contacto?slug=redes-y-contacto"
+        const url =
+          "https://admin.ciberespacioartistico.com/index.php/wp-json/wp/v2/contacto?slug=redes-y-contacto";
         const response = await fetch(url);
         const jsonData = await response.json();
         setData(jsonData[0]);
       } catch (error) {
-        console.log('Error al obtener los datos de la API:', error);
+        console.log("Error al obtener los datos de la API:", error);
       }
-    };
+    }
 
     fetchData();
-  }, [])
+  }, []);
 
   return (
     <footer className={`${styles.footer} ${rubik.className}`}>
@@ -32,8 +32,11 @@ export default function Footer () {
           <h4>Síguenos</h4>
           <ul className={styles.logos_container}>
             <li>
-              <a href={data ? data.ACF.redes_sociales.instagram : "/"} target="_blank">
-                <Image 
+              <a
+                href={data ? data.ACF.redes_sociales.instagram : "/"}
+                target="_blank"
+              >
+                <Image
                   src="/social/instagram-morado.svg"
                   width={50}
                   height={50}
@@ -42,8 +45,11 @@ export default function Footer () {
               </a>
             </li>
             <li>
-              <a href={data ? data.ACF.redes_sociales.youtube : "/"} target="_blank">
-                <Image 
+              <a
+                href={data ? data.ACF.redes_sociales.youtube : "/"}
+                target="_blank"
+              >
+                <Image
                   src="/social/youtube-morado.svg"
                   width={50}
                   height={50}
@@ -52,8 +58,11 @@ export default function Footer () {
               </a>
             </li>
             <li>
-              <a href={data ? data.ACF.redes_sociales.youtube : "/"} target="_blank">
-                <Image 
+              <a
+                href={data ? data.ACF.redes_sociales.youtube : "/"}
+                target="_blank"
+              >
+                <Image
                   src="/social/twitter-morado.svg"
                   width={50}
                   height={50}
@@ -65,7 +74,7 @@ export default function Footer () {
         </li>
 
         <li className={styles.second_container}>
-        <h4>Escríbenos</h4>
+          <h4>Escríbenos</h4>
           <ul>
             <li className={styles.email_container}>
               <Link href="/vinculate" className="my-link">
@@ -76,6 +85,22 @@ export default function Footer () {
           </ul>
         </li>
       </ul>
+      <div className={styles.author_container}>
+        <p style={{ fontStyle: "italic" }}>Developed with Love: </p>
+        <p>
+          {" "}
+          <Link
+            href="https://juanfe.bio.link/"
+            alt="Sitio Hecho Por Juan Felipe Parra Runceria - Desarrollador Web"
+            className={styles.author_link}
+            target="_blank"
+          >
+            Juan Felipe Parra Runceria
+          </Link>
+          {""}
+        </p>
+        <p>2023</p>
+      </div>
     </footer>
-  )
+  );
 }
