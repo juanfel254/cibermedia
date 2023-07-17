07f3a48edb1e413e7d7c339a0db236f410d67b40
juanfel254 <jparrar@unal.edu.co>
2023-07-17 12:19:31 -0500

    Add gallery component to artist portfolio section

---
+++ src/components/artistProjects/galleryProject.js
@@ -0,0 +1,35 @@
+import styles from "@/styles/pages-styles/artista.module.css";
+import Carousel from "react-gallery-carousel";
+import "react-gallery-carousel/dist/index.css";
+
+export default function GalleryProject({ projectInfo }) {
+  const galleryUrls = Object.keys(projectInfo.ACF.galeria_de_fotos)
+    .filter((key) => projectInfo.ACF.galeria_de_fotos[key] !== false)
+    .map((key) => ({ src: projectInfo.ACF.galeria_de_fotos[key] }));
+
+  return (
+    <div className={styles.gallery_project_container}>
+      <div className={styles.gallery_container}>
+        <Carousel
+          className="gallery-carousel"
+          images={galleryUrls}
+          hasMediaButton={false}
+          hasIndexBoard={false}
+          hasThumbnails={true}
+          style={{
+            backgroundColor: "var(--light-black-opacity",
+            aspectRatio: "auto",
+          }}
+        />
+      </div>
+      <div className={styles.gallery_info}>
+        <p className={styles.gallery_description}>
+          {projectInfo.ACF.descripcion_galeria}
+        </p>
+        <p className={styles.gallery_credits}>
+          {projectInfo.ACF.creditos_galeria}
+        </p>
+      </div>
+    </div>
+  );
+}
